abc5a5a645571597df4cf22a350794468578e7b4
jarrod2022 <hwang.jarrod@gmail.com>
2023-05-01 01:38:26 -0700

    Add project files.

---
+++ MainForm.cpp
@@ -0,0 +1,22 @@
+#include "MainForm.h"
+
+
+using namespace System;
+
+using namespace System::Windows::Forms;
+
+[STAThread]
+
+void main(array<String^>^ args)
+
+{
+
+    Application::EnableVisualStyles();
+
+    Application::SetCompatibleTextRenderingDefault(false);
+
+    bxstd::MainForm form;
+
+    Application::Run(% form);
+
+}

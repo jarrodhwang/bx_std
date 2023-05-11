79bffb9318d64d46b1f7f18c4e297bb7b116a7f2
jarrod2022 <hwang.jarrod@gmail.com>
2023-05-11 16:56:27 +0900

    db connetor shema

--- DBconnector.h
+++ DBconnector.h
@@ -1,12 +1,138 @@
-#pragma once
+﻿#pragma once
 
 #include <string>
 #include <vector>
 #include <iostream>
 #include <mysql.h>
 
+#include "User.h"
+#include "Product.h"
+#include "Bin.h"
+#include "Box.h"
+#include "Monitoring.h"
+#include "LoadCell.h"
+#include "Scale.h"
+
+using namespace std;
 
 class DBconnector
 {
+
+private :
+
+
+
+
+	MYSQL* connection;
+	MYSQL* conn_result;
+	unsigned int timeout_sec = 1;
+
+
+
+	// user info 
+	User select_user(const User& usr);
+	User insert_user(const User& usr);
+	User update_user(const User& usr);
+	//t_bin		>>	user_id
+	User delete_user(const User& usr);
+
+
+	//product info
+	Product select_product(const Product& prt);
+	Product insert_product(const Product& prt);
+	Product update_product(const Product& prt);
+	// t_bin	>>	product_id
+	Product delete_product(const Product& prt);
+
+
+	//scale info
+	Scale select_scale(const Scale& scl);
+	Scale insert_scale(const Scale& scl);
+	Scale update_scale(const Scale& scl);
+	Scale delete_scale(const Scale& scl);
+
+	//bin info
+	Bin select_bin(const Bin& bin);
+	Bin insert_bin(const Bin& bin);
+	Bin update_bin(const Bin& bin);
+	Bin delete_bin(const Bin& bin);
+
+	//box info
+	Box select_box(const Box& box);
+	Box insert_box(const Box& box);
+	Box update_box(const Box& box);
+	Box delete_box(const Box& box);
+
+	////Area info
+	//Area select_scale(const Scale& scl);
+	//Area insert_scale(const Scale& scl);
+	//Area update_scale(const Scale& scl);
+	//Area delete_scale(const Scale& scl);
+
+	////scale info
+	//Factory select_scale(const Scale& scl);
+	//Factory insert_scale(const Scale& scl);
+	//Factory update_scale(const Scale& scl);
+	//Factory delete_scale(const Scale& scl);
+
+
+	bool connect();
+	bool disconnect();
+	bool execute(const string query);
+
+
+public:
+	//select, insert, update, delete
+	// timestamp
+	
+	//타입과 생성자 파라미터 수정 필요
+	DBconnector(const string ip_addr, int port, string id, string pw, string db_name );
+	~DBconnector();
+
+	/*
+	* 로그인 시, 로그인 정보 가져와서 company return
+	* TIMESTAMP, log 
+	*/
+	Company login(const Login& info);
+
+	/*
+	* 
+	* 차후에 area, factory 관련 쿼리및 구조 작성 해주면 됨
+	*/
+
+
+	/*
+	* 지금은 area_uid, factory_id, company_id 고정적으로 들어감
+	*/
+
+	vector<Box> selectBox(const Area& area_uid);
+	bool insertBox(const Area& area_uid,const Box& box_info);
+	bool updateBox(const Area& area_uid, const Box& box_info);
+	// box_id를 들고 있는 t_bin, t_scale 유의
+	bool deleteBox(const Area& area_uid, const Box& box_info);
+
+
+	
+
+
 };
 
+
+
+//void main()
+//{
+//	MYSQL* conn;
+//	MYSQL* conn_result;
+//	unsigned int timeout_sec = 1;
+//	
+//	conn = mysql_init(NULL);
+//	mysql_options(conn, MYSQL_OPT_CONNECT_TIMEOUT, &timeout_sec);
+//	conn_result = mysql_real_connect(conn, "127.0.0.1", "root", "1618", "boxcon_std", 3306, NULL, 0);
+//
+//	if (NULL == conn_result)
+//	{
+//		cout << "DB Connection Fail" << endl;
+//	}
+//	else
+//		cout << "DB connector Success" << endl;
+//}
---
+++ Scale.h
@@ -0,0 +1,112 @@
+﻿#pragma once
+#pragma once
+#pragma once
+/*
+* Scale.h
+*
+* Description : Implementation of Scale class, it contains IoT ID, IP, User object, Product Object, notification cycle
+*
+* 설명 : 빈 클래스
+*		보드 아이디, 아이피, 사용자 객체, 제품 객체, 알림 주기 등의 정보를 담고 있음
+*
+* Author : Seongjae Hwang, 황성재
+* Date : 03. May. 2023, 2023년 05월 03일
+*/
+
+#ifndef SCALE_H
+#define SCALE_H
+
+#include "User.h"
+#include "Product.h"
+#include <string>
+#include <vector>
+
+using namespace std;
+
+
+class Scale {
+
+private:
+	string ip_addr;
+	int port;
+	vector<User> userList;
+	Product product;
+	int ScaleID;
+	int boxID;
+	int alarmCycle;
+	double ScaleWeight;
+
+
+
+
+public:
+
+	struct ScaleState
+	{
+		bool IsConnected = false;
+		bool IsLoaded = false;
+		bool IsUnderSecureStock = false;
+
+		bool HasIpAddr = false;
+		bool HasPort = false;
+		bool HasScaleID = false;
+		bool HasBoxID = false;
+		bool HasUserL = false;
+		bool HasAlarmCycle = false;
+	};
+
+	ScaleState state;
+
+	// Constructor
+	Scale(const string& ip_addr,
+		const int& port,
+		const vector<User>& userL,
+		const Product& product,
+		const int& ScaleID,
+		const int& boxID,
+		const int& alarmCyc,
+		const double& ScaleW);
+
+
+	// Destructor
+	~Scale();
+
+
+	// getter and setter
+	string getIpAddress() const;
+	int getPort() const;
+	vector<User> getUserList() const;
+	Product getProduct() const;
+	int getScaleID() const;
+	int getBoxID() const;
+	int getAlarmCycle() const;
+	double getScaleWeight() const;
+	ScaleState getScaleState() const;
+
+
+
+	bool setIpAddress(const string& ip_addr);
+	bool setPort(const int& port);
+	bool setUserList(const vector<User>& userList);
+	bool setProduct(const Product& product);
+	bool setScaleID(const int& ScaleID);
+	bool setBoxID(const int& boxID);
+	bool setAlarmCycle(const int& alarmCyc);
+	bool setScaleWeight(const double& ScaleW);
+	bool setScaleState(const ScaleState& st);
+
+	//  functions
+	bool IsValidIpAddress(const string& ip);
+	bool IsValidUserList(const vector<User>& userL);
+	bool IsValidProduct(const Product& product);
+	bool IsValidScaleID(const int& ScaleID);
+	bool IsValidBoxID(const int& boxID);
+
+	int countScaleInventory();
+	void toleranceCalculation();
+
+
+
+
+};
+#endif
